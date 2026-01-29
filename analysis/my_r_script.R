96ce9020925dc8ef6753dd2c3fd763ff10f748e7
Gia Vollmer <giavoll01@gmail.com>
2026-01-29 15:25:08 -0600

    adding first r script

---
+++ analysis/my_r_script.R
@@ -0,0 +1,46 @@
+# this is a comment
+100 + 101
+
+# CTRL + Enter to run all lines in the console 
+3 + 5 * 10
+
+# help me remember order of operations 
+3 + (5 * 10)
+
+# I want to add first
+(3+5)*10
+
+# Do I need a multiplication symbol? yes
+
+
+# exponent 
+3^2
+
+
+#what do fractions look like?
+2/1000000
+
+# R has functions 
+getwd()
+sin(1)
+?sin
+
+# R is like math
+x <- 1/40
+x = 1/100
+
+# R has vectors
+1:5
+x <- 3:10
+
+
+# vectors can be used in computations
+2 ^ (x)
+#to remove vectors
+rm(x)
+
+# install packages 
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("dplyr")
+library(dplyr)
